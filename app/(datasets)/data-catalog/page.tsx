9ceb405a13b0567803784e7990e0c221afe4e802
Hanbyul Jo <hanbyul@nyu.edu>
2025-03-10 10:53:29 -0400

    Make each page consistent

--- app/(datasets)/data-catalog/page.tsx
+++ app/(datasets)/data-catalog/page.tsx
@@ -3,23 +3,22 @@ import dynamic from 'next/dynamic';
 import { getTransformedDatasetMetadata } from 'app/content/utils/mdx';
 
 // @NOTE: Dynamically load to ensure only CSR since this depends ContextProviders for routing and etc...
-const Catalog = dynamic(
-  () => import('./catalog'),
-  { 
-    ssr: false,
-    loading: () => <p>Loading...</p> // @NOTE @TODO: We need a loading state!!!
-  },
-);
+const Catalog = dynamic(() => import('./catalog'), {
+  ssr: false,
+  loading: () => <p>Loading...</p>, // @NOTE @TODO: We need a loading state!!!
+});
 
 export default function Page() {
   const transformed = getTransformedDatasetMetadata();
 
   return (
-    <div className='grid-container'>
-      <section>
-      <h1 className='font-ui-lg'>Data Catalog</h1>
+    <section>
+      <div className='grid-container'>
+        <div className='grid-row margin-top-5'>
+          <h1 className='font-ui-lg'>Data Catalog</h1>
+        </div>
         <Catalog datasets={transformed} />
-      </section>
-    </div>
+      </div>
+    </section>
   );
 }
--- app/(datasets)/stories/page.tsx
+++ app/(datasets)/stories/page.tsx
@@ -3,13 +3,10 @@ import dynamic from 'next/dynamic';
 import { getStoriesMetadata } from 'app/content/utils/mdx';
 
 // @NOTE: Dynamically load to ensure only CSR since these depends on VedaUI ContextProvider for routing...
-const StoriesHub = dynamic(
-  () => import('./hub'),
-  { 
-    ssr: false,
-    loading: () => <p>Loading...</p> // @NOTE @TODO: We need a loading state!!!
-  },
-);
+const StoriesHub = dynamic(() => import('./hub'), {
+  ssr: false,
+  loading: () => <p>Loading...</p>, // @NOTE @TODO: We need a loading state!!!
+});
 
 export default function Page() {
   const stories = getStoriesMetadata().map((d) => ({
@@ -19,7 +16,9 @@ export default function Page() {
 
   return (
     <div className='grid-container'>
-      <h1 className='font-ui-lg'>Stories</h1>
+      <div className='grid-row margin-top-5'>
+        <h1 className='font-ui-lg'>Stories</h1>
+      </div>
       <StoriesHub stories={stories} />
     </div>
   );
