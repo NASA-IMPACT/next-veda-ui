a6bd7d5afd6006bcf6b830fb82e998314be3f2b0
Hanbyul Jo <hanbyul@nyu.edu>
2025-03-25 19:28:28 +0900

    Fix package json

--- app/store/providers/veda-ui-config.tsx
+++ app/store/providers/veda-ui-config.tsx
@@ -11,7 +11,6 @@ export default function VedaUIConfigProvider({ children }: { children: any }) {
         envApiStacEndpoint: process.env.NEXT_PUBLIC_API_STAC_ENDPOINT ?? '',
         envApiRasterEndpoint: process.env.NEXT_PUBLIC_API_RASTER_ENDPOINT ?? '',
         navigation: {
-          // @ts-expect-error until veda-ui fixes types
           LinkComponent: Link,
           linkProps: {
             pathAttributeKeyName: 'href',
