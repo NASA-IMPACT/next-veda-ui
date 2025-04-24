a82a8b5371ffb6944b510fb1147f1a7f88d3ca8f
Alice Rühl <alice@developmentseed.org>
2025-04-24 18:09:46 +0200

    Replace FooterNav with div and desktop:flex-row
    Seemed easier to replicate the design that way

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {
   Footer as USWDSFooter,
-  FooterNav,
-  Address,
-  Logo,
   GridContainer,
   Grid,
 } from '@trussworks/react-uswds';
@@ -20,18 +17,19 @@ export default function Footer() {
           <Grid row>
             <h3>Earth.gov</h3>
           </Grid>
-          <Grid row>
-            <Grid col={4}>
-              <p className='text-base-light measure-1'>
+          <Grid
+            row
+            className='display-flex flex-column desktop:flex-row flex-justify'
+          >
+            <div>
+              <p className='text-base-light font-body-2xs measure-1'>
                 The EIC was created by NASA and is enabled by contributions
                 across EPA, FEMA, NASA, NOAA, USAID, USDA and USGS.
               </p>
-            </Grid>
-            <Grid col={'fill'}></Grid>
-            <Grid col={7}>
-              <FooterNav
-                size='slim'
-                links={[
+            </div>
+            <div>
+              <nav className='display-flex flex-column desktop:flex-row'>
+                {[
                   { label: 'About', href: '/about' },
                   { label: 'Dashboard', href: '/dashboard' },
                   { label: 'Mapping App', href: '/dashboard' },
@@ -40,60 +38,70 @@ export default function Footer() {
                   { label: 'Contact Us', href: '/contact' },
                 ].map(({ label, href }) => (
                   <Link
-                    className='usa-footer__primary-link text-white text-no-underline font-body-2xs'
+                    className='usa-footer__primary-link padding-0 desktop:padding-2 desktop:padding-bottom-205 text-white text-no-underline text-normal line-height-ui-6 font-ui-2xs'
                     href={href}
                     key={href}
                   >
                     {label}
                   </Link>
                 ))}
-              />
-            </Grid>
+              </nav>
+            </div>
           </Grid>
         </GridContainer>
       }
       secondary={
         <>
-          <Grid row gap={4}>
-            {[
-              { label: 'Accessibility support', href: '/' },
-              { label: 'No FEAR Act data', href: '/' },
-              { label: 'Office of the Inspector General', href: '/' },
-              { label: 'FOIA requests', href: '/' },
-              { label: 'Performance reports', href: '/' },
-              { label: 'Privacy policy', href: '/' },
-            ].map(({ label, href }) => (
-              <Grid key={href} col className='flex-auto'>
-                <a href={href} className='text-base-light font-body-2xs'>
-                  {label}
-                </a>
-              </Grid>
-            ))}
+          {/* This is already a grid container in trusswork's implementation*/}
+          <Grid row className='border-top-1px border-base-darkest padding-y-2'>
+            <div className='display-flex flex-column desktop:flex-row'>
+              {[
+                { label: 'Accessibility support', href: '/' },
+                { label: 'No FEAR Act data', href: '/' },
+                { label: 'Office of the Inspector General', href: '/' },
+                { label: 'FOIA requests', href: '/' },
+                { label: 'Performance reports', href: '/' },
+                { label: 'Privacy policy', href: '/' },
+              ].map(({ label, href }) => (
+                <div key={href}>
+                  <a
+                    href={href}
+                    className='text-base-light font-body-2xs padding-right-4'
+                  >
+                    {label}
+                  </a>
+                </div>
+              ))}
+            </div>
           </Grid>
+
+          {/* Third section */}
           <Grid
             row
             gap={4}
-            className='border-top-1px border-base-darkest margin-top-4 padding-top-2 text-base-light font-body-2xs'
+            className='desktop:flex-row flex-justify border-top-1px border-base-darkest padding-top-2 text-base-light font-body-2xs'
           >
-            <Grid tablet={{ col: 4 }}>
+            <div className='desktop:display-flex desktop:flex-row flex-justify desktop:order-last'>
+              <div className='padding-right-4'>
+                <p>
+                  Page last updated:{' '}
+                  <span className='text-white'>Mar 26, 2024</span>
+                </p>
+              </div>
+              <div>
+                <p>
+                  U.S. Earth Information Center Responsible Official: Eleanor
+                  Stokes
+                </p>
+              </div>
+            </div>
+
+            <div>
               <p>
                 Looking for U.S. government information and services? Visit
                 USA.gov
               </p>
-            </Grid>
-            <Grid tablet={{ col: 2 }}></Grid>
-            <Grid tablet={{ col: 2 }}>
-              <p>
-                Page last updated:{' '}
-                <span className='text-white'>Mar 26, 2024</span>
-              </p>
-            </Grid>
-            <Grid tablet={{ col: 4 }}>
-              <p>
-                U.S. Earth Information Center Responsible Official: Eleanor
-                Stokes
-              </p>
-            </Grid>
+            </div>
           </Grid>
         </>
       }
