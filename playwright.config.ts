70f06dbb79eea9f607a6af3d7a00412cc15839a4
Vitor George <vitor.george@gmail.com>
2025-04-30 13:16:48 +0100

    fix: increase playwright timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,9 +8,14 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   reporter: 'html',
+  timeout: 60000,
+  expect: {
+    timeout: 10000,
+  },
   use: {
     baseURL: 'http://127.0.0.1:3000',
     trace: 'on-first-retry',
+    navigationTimeout: 60000,
   },
   projects: [
     {
