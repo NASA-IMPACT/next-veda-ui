c291bc98e4e7ff0c3e54747022f2a8836c7c1911
Sandra Hoang <sandrahoang686@gmail.com>
2025-05-06 16:34:35 -0400

    fix accessibility playwright errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,39 @@
-'use client';
 import React from 'react';
+import type { Metadata } from 'next';
+import { baseUrl } from './sitemap';
 import './styles/index.scss';
 import '@teamimpact/veda-ui/lib/main.css';
 
 import Header from './components/header';
 import Footer from './components/footer';
+
+export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl ?? ''),
+  title: {
+    default: 'Earth Information Center',
+    template: '%s | Earth Information Center',
+  },
+  description: 'Explore our changing planet.',
+  openGraph: {
+    title: 'Earth Information Center',
+    description: 'Explore our changing planet.',
+    url: baseUrl,
+    siteName: 'Earth Information Center',
+    locale: 'en_US',
+    type: 'website',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
