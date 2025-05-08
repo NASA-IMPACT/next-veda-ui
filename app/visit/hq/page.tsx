f48fdd308ba0ab58eae2d30b59bb37892c59f58a
Alice Rühl <alice@developmentseed.org>
2025-05-08 10:44:23 +0200

    Add full-width separator lines
    using <hr> to signal a visual or thematic break between parts of content

--- app/visit/hq/page.tsx
+++ app/visit/hq/page.tsx
@@ -2,22 +2,39 @@ import React from 'react';
 import { Grid, GridContainer } from '@trussworks/react-uswds';
 
 import { Paragraph } from 'app/components/common/Paragraph';
+import Separator from 'app/components/common/Separator';
 
 const HqPage: React.FC = () => {
   return (
-    <GridContainer containerSize='desktop-lg' className='desktop:padding-y-10'>
-      <Grid row className='margin-bottom-4 measure-1 desktop:measure-4'>
-        <h1 className='text-uppercase'>NASA HQ</h1>
-        <Paragraph>
-          The Earth Information Center at HQ is a physical and virtual
-          experience at NASA Headquarters, where visitors can see how our planet
-          is changing in areas that affect lives and livelihoods– from
-          temperatures in our cities to sea level rise, greenhouse gas emissions
-          to agricultural productivity.
-        </Paragraph>
-      </Grid>
-      <Grid row>{/* Add other content here! */}</Grid>
-    </GridContainer>
+    <>
+      <GridContainer
+        containerSize='desktop-lg'
+        className='desktop:padding-top-10'
+      >
+        <Grid row className='margin-bottom-4 measure-1 desktop:measure-4'>
+          <h1 className='text-uppercase'>NASA HQ</h1>
+          <Paragraph>
+            The Earth Information Center at HQ is a physical and virtual
+            experience at NASA Headquarters, where visitors can see how our
+            planet is changing in areas that affect lives and livelihoods– from
+            temperatures in our cities to sea level rise, greenhouse gas
+            emissions to agricultural productivity.
+          </Paragraph>
+        </Grid>
+      </GridContainer>
+
+      <Separator />
+
+      <GridContainer containerSize='desktop-lg'>
+        <Grid row>{/* Add other content here! */}</Grid>
+      </GridContainer>
+
+      <Separator />
+
+      <GridContainer containerSize='desktop-lg'>
+        <Grid row>{/* Add other content here! */}</Grid>
+      </GridContainer>
+    </>
   );
 };
 
