b9af412ca43f51c44d65f31c7433e89d3aee00d6
Vitor George <vitor.george@gmail.com>
2025-05-08 12:53:35 +0100

    feat: add visit center cards using USWDS react library

--- app/visit/page.tsx
+++ app/visit/page.tsx
@@ -1,7 +1,84 @@
 import React from 'react';
-import { Grid, GridContainer } from '@trussworks/react-uswds';
-
+import {
+  Grid,
+  GridContainer,
+  Card,
+  CardMedia,
+  CardHeader,
+  CardBody,
+} from '@trussworks/react-uswds';
 import { Paragraph } from 'app/components/common/Paragraph';
+import Image from 'next/image';
+
+interface CenterCard {
+  key: string;
+  heading: string;
+  openingHours: string[];
+  address: string[];
+  imgSrc: string;
+  imgAlt: string;
+}
+
+const centerCards: CenterCard[] = [
+  {
+    key: 'nasa-hq',
+    heading: 'NASA HQ',
+    openingHours: ['Monday - Friday', '8:30am - 5:30pm'],
+    address: [
+      'NASA Headquarters (East Lobby)',
+      '300 E St. SW, Washington, DC 20546',
+    ],
+    imgSrc: '/images/visit/nasa-hq.png',
+    imgAlt: 'NASA HQ exhibit view',
+  },
+  {
+    key: 'smithsonian-museum',
+    heading: 'Smithsonian National Museum of Natural History',
+    openingHours: ['Every day (closed on Christmas)', '10am - 5:30pm'],
+    address: [
+      'Smithsonian National Museum of Natural History',
+      '1000 Madison Dr, Washington, DC 20560',
+    ],
+    imgSrc: '/images/visit/smithsoniam-museum.jpg',
+    imgAlt: 'Smithsonian National Museum of Natural History exterior view',
+  },
+  {
+    key: 'kennedy-space-center',
+    heading: 'Kennedy Space Center',
+    openingHours: ['Every day (closed on Christmas)', '9am - 5pm'],
+    address: [
+      'KSC Visitor Complex (The Gantry at LC-39)',
+      'Space Commerce Way, Merritt Island, FL 32953',
+    ],
+    imgSrc: '/images/visit/kennedy-space-center.png',
+    imgAlt: 'Kennedy Space Center exterior view',
+  },
+];
+
+const renderCenterCard = (card: CenterCard) => (
+  <Card key={card.key}>
+    <CardHeader>
+      <h2 className='margin-top-0'>{card.heading}</h2>
+    </CardHeader>
+    <CardMedia>
+      <Image width={300} height={300} src={card.imgSrc} alt={card.imgAlt} />
+    </CardMedia>
+    <CardBody>
+      <h3 className='margin-bottom-1'>Hours</h3>
+      <ul className='usa-list'>
+        {card.openingHours.map((hour, index) => (
+          <li key={index}>{hour}</li>
+        ))}
+      </ul>
+      <h3 className='margin-bottom-1'>Address</h3>
+      <ul className='usa-list'>
+        {card.address.map((line, index) => (
+          <li key={index}>{line}</li>
+        ))}
+      </ul>
+    </CardBody>
+  </Card>
+);
 
 const VisitPage: React.FC = () => {
   return (
@@ -15,7 +92,7 @@ const VisitPage: React.FC = () => {
           Florida). All exhibits are open to the public.
         </Paragraph>
       </Grid>
-      <Grid row>{/* Add center cards here! */}</Grid>
+      <Grid row>{centerCards.map(renderCenterCard)}</Grid>
     </GridContainer>
   );
 };
