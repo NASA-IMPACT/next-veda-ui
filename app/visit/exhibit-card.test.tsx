2d8b29a18f031c440e30b596cf9ce8c0c69ef420
Vitor George <vitor.george@gmail.com>
2025-05-12 10:01:17 +0100

    fix: add more test coverage

--- app/visit/exhibit-card.test.tsx
+++ app/visit/exhibit-card.test.tsx
@@ -12,4 +12,29 @@ describe('Exhibit Card', () => {
     const cardElement = screen.getByTestId('Card');
     expect(cardElement).toMatchSnapshot();
   });
+
+  it('should render the exhibit card', () => {
+    render(<ExhibitCard exhibit={exhibits[0]} />);
+    expect(screen.getByText(exhibits[0].heading)).toBeInTheDocument();
+    expect(screen.getByText(exhibits[0].address[0])).toBeInTheDocument();
+    expect(screen.getByText(exhibits[0].address[1])).toBeInTheDocument();
+    expect(screen.getByText(exhibits[0].openingHours[0])).toBeInTheDocument();
+  });
+
+  it('should render the exhibit card with the correct image', () => {
+    render(<ExhibitCard exhibit={exhibits[0]} />);
+    const imageElement = screen.getByRole('img');
+    const encodedImgSrc = encodeURIComponent(exhibits[0].imgSrc);
+    expect(imageElement.getAttribute('src')).toContain(encodedImgSrc);
+    expect(imageElement).toHaveAttribute('alt', exhibits[0].imgAlt);
+  });
+
+  it('should render the exhibit card with the correct link', () => {
+    render(<ExhibitCard exhibit={exhibits[0]} />);
+    const linkElement = screen.getByRole('link');
+    expect(linkElement).toHaveAttribute(
+      'href',
+      `/visit/exhibit/${exhibits[0].id}`,
+    );
+  });
 });
