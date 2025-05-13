3df7b52e284391af62b261b9431a28d2a3714aed
Vitor George <vitor.george@gmail.com>
2025-05-13 15:06:04 +0100

    feat: scaffold theme cards

---
+++ app/components/common/cards/small/theme/index.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+
+import ThemeCard from './index';
+import { DATA_THEMES } from 'app/config';
+
+describe('Theme Card', () => {
+  const testTheme = DATA_THEMES[0];
+
+  it('matches the snapshot', () => {
+    render(<ThemeCard theme={testTheme} />);
+    const cardElement = screen.getByTestId('Card');
+    expect(cardElement).toMatchSnapshot();
+  });
+
+  it('should render the theme card', () => {
+    render(<ThemeCard theme={testTheme} />);
+    expect(screen.getByText(testTheme.title)).toBeInTheDocument();
+  });
+
+  it('should render the theme card with the correct image', () => {
+    render(<ThemeCard theme={testTheme} />);
+    const imageElement = screen.getByRole('img');
+    const encodedImgSrc = encodeURIComponent(testTheme.cardImage);
+    expect(imageElement.getAttribute('src')).toContain(encodedImgSrc);
+    expect(imageElement).toHaveAttribute(
+      'alt',
+      `${testTheme.title} theme cover image`,
+    );
+  });
+
+  it('should render the theme card with the correct link', () => {
+    render(<ThemeCard theme={testTheme} />);
+    const linkElement = screen.getByRole('link');
+    expect(linkElement).toHaveAttribute('href', `/themes/${testTheme.id}`);
+  });
+});
---
+++ app/components/common/cards/small/theme/index.tsx
@@ -0,0 +1,24 @@
+'use client';
+import React from 'react';
+import { Icon } from '@trussworks/react-uswds';
+import SmallCard from '../index';
+import type { Theme } from 'app/config';
+
+export const ThemeCard: React.FC<{ theme: Theme }> = ({ theme }) => {
+  return (
+    <SmallCard
+      id={theme.id}
+      heading={theme.title}
+      imgSrc={theme.cardImage}
+      imgAlt={`${theme.title} theme cover image`}
+      linkHref={`/themes/${theme.id}`}
+      linkLabel={`View ${theme.title} theme details.`}
+      badge={{
+        icon: Icon.Lightbulb,
+        text: 'Theme',
+      }}
+    />
+  );
+};
+
+export default ThemeCard;
