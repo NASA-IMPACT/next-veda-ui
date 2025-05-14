8770b437ff80bd3e590c54fa6b3b6d581d0187d6
Alice Rühl <alice@developmentseed.org>
2025-05-14 10:54:48 +0200

    Remove SideNav from mdx-components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,12 +1,9 @@
 import type { MDXComponents } from 'mdx/types';
 import { SafeLink } from 'app/components/mdx-components/safe-link';
 
-import { SideNav } from '@trussworks/react-uswds';
-
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
     a: SafeLink,
-    SideNav,
   };
 }
