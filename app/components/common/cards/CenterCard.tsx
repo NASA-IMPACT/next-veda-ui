26ffd601955784a04fa313f81a6775fb81e59530
Vitor George <vitor.george@gmail.com>
2025-06-27 12:03:58 +0100

    fix: use a fixed size for images in center card

--- app/components/common/cards/CenterCard.tsx
+++ app/components/common/cards/CenterCard.tsx
@@ -16,7 +16,7 @@ interface CenterCardProps {
 export const CenterCard: React.FC<CenterCardProps> = ({ center, isWide }) => {
   return (
     <div
-      className={`center-card bg-ink border-1px border-base-darkest radius-md padding-y-5 padding-x-3 ${isWide ? 'is-first' : 'is-secondary'}`}
+      className={`center-card bg-ink border-1px border-base-darkest radius-md padding-y-5 padding-x-3`}
     >
       {isWide && (
         <div className='center-card__image'>
