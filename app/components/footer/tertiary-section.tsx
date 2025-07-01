7f4d8f600b9caf0a23b19c3c7b15c3379309f3c2
Vitor George <vitor.george@gmail.com>
2025-07-01 11:00:16 +0100

    Revert "fix: change element order for correct keyboard navigation"
    This reverts commit dadcf2a8d9fc2e6e60ed1a85b7e13f21fbbe5d08.

--- app/components/footer/tertiary-section.tsx
+++ app/components/footer/tertiary-section.tsx
@@ -7,19 +7,6 @@ const TertiarySection = () => {
       row
       className='border-top-1px border-base-darkest padding-top-2 desktop:display-flex flex-column desktop:flex-row text-base-light font-body-2xs'
     >
-      <div className='desktop:display-flex desktop:order-first'>
-        <p className='margin-top-0 measure-1 desktop:measure-4'>
-          Looking for U.S. government information and services?{' '}
-          <Link
-            href='https://www.usa.gov'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Visit USA.gov
-          </Link>
-        </p>
-      </div>
-
       <div className='desktop:display-flex desktop:flex-row desktop:order-last'>
         <div className='padding-right-4'>
           <p className='margin-top-0 measure-1 desktop:measure-4'>
@@ -35,6 +22,19 @@ const TertiarySection = () => {
             <Link href='mailto:eleanor.stokes@nasa.gov'>Eleanor Stokes</Link>
           </p>
         </div>
+      </div>
+
+      <div className='desktop:display-flex desktop:order-first'>
+        <p className='margin-top-0 measure-1 desktop:measure-4'>
+          Looking for U.S. government information and services?{' '}
+          <Link
+            href='https://www.usa.gov'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Visit USA.gov
+          </Link>
+        </p>
       </div>
     </Grid>
   );
